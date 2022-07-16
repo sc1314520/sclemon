6427ea1ea31ecd3eba93e4eb8a0bd9901a4f4bfd
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-07-17 01:18:05 +0800

    s

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.home').hover(function (event) {
+        event.preventDefault();
+        $('.home-text').toggleClass("animate__lightSpeedInLeft");
+      });
+    $('.about').hover(function (event) {
+        event.preventDefault();
+        $('.about h1').toggleClass("animate__bounceIn");
+      });
+      $('.skills').hover(function (event) {
+        event.preventDefault();
+        $('.skills h1').toggleClass("animate__bounceIn");
+      });
+      $('.project').hover(function (event) {
+        event.preventDefault();
+        $('.project>h1').toggleClass("animate__bounceIn");
+        $('.content').addClass("animate__fadeIn");
+      });
+});
