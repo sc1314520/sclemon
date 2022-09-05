07f5e10750d2d65fccee1d0160af4d128bb27bbd
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-09-05 23:07:03 +0800

    s

--- js/index.js
+++ js/index.js
@@ -20,6 +20,7 @@ $(document).ready(function () {
         $('.project>h1').toggleClass("animate__bounceIn");
         $('.content').addClass("animate__fadeIn");
       });
+
       $('.ocpjp-click').click(function (event) {
         event.preventDefault();
         $('.ocp-open').addClass("ocp-open-2");
@@ -28,6 +29,7 @@ $(document).ready(function () {
         event.preventDefault();
         $('.ocp-open').toggleClass("ocp-open-2");
       });
+
       $('.fans-click').click(function (event) {
         event.preventDefault();
         $('.fans-open').addClass("fans-open-2");
@@ -35,6 +37,15 @@ $(document).ready(function () {
       $('.close-button2').click(function (event) {
         event.preventDefault();
         $('.fans-open').toggleClass("fans-open-2");
+      });
+
+      $('.ntnu-click').click(function (event) {
+        event.preventDefault();
+        $('.ntnu-open').addClass("ntnu-open-2");
+      });
+      $('.close-button3').click(function (event) {
+        event.preventDefault();
+        $('.ntnu-open').toggleClass("ntnu-open-2");
       });
 });
 function VS(){
