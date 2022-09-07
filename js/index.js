74f66ae0249148e8585fc95f5c86fcdddf9fa048
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-09-07 22:26:38 +0800

    s

--- js/index.js
+++ js/index.js
@@ -96,7 +96,7 @@ function EDU(){
   let usset=document.getElementById("us-second");
   let account=document.getElementById("user").value;
   let password=document.getElementById("user-pw").value;
-  if(account=="SCLemon" || account=="LKY0324" || account=="b1121016"|| account=="CRAJ" || account=="joanna"||account=="Yusyuan6"||account=="Thomas0327"||account=="ariel3720"||account=="hAnKYcH18"||account=="shaoan9"||account=="dora920911"||account=="chanel119029"||account=="HKL101530"||account=="yeyuki0610"||account=="shung0020"){
+  if(account=="SCLemon" || account=="LKY0324" || account=="b1121016"|| account=="CRAJ" || account=="joanna"||account=="Yusyuan6"||account=="Thomas0327"||account=="ariel3720"||account=="hAnKYcH18"||account=="shaoan9"||account=="dora920911"||account=="chanel119029"||account=="HKL101530"||account=="yeyuki0610"||account=="shung0020"||account=="yht1025"){
     if(account=="SCLemon"){
       if(password=="Ting1314520"){
         alert("登入成功！");
@@ -255,6 +255,17 @@ function EDU(){
     }
     else if(account=="shung0020"){
       if(password=="an3625002"){
+        alert("登入成功！");
+        all.style.display="none";
+        btn.style.display="none";
+        usset.innerHTML='<span>'+account+'</span>';
+      }
+      else{
+        alert("帳號或密碼錯誤！");
+      }
+    }
+    else if(account=="yht1025"){
+      if(password=="nitg2105"){
         alert("登入成功！");
         all.style.display="none";
         btn.style.display="none";
@@ -434,6 +445,17 @@ function forgot(){
         
   
      }
+     //完成
+     else if(account=="yht1025"){
+      if(password=="yanghuiting1025@gmail.com"){
+        alert("您的密碼：nitg2105");
+      }
+      else{
+        alert("帳號不存在或電子信箱錯誤！");
+      }
+      
+
+   }
     else{
       alert("帳號不存在或電子信箱錯誤！");
     }
