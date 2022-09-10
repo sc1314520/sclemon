6e40af23aaf98f095dd61513ff922d5c8f11ae8e
SC1314520 <104412070+SC1314520@users.noreply.github.com>
2022-09-10 17:47:54 +0800

    s

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
     $('.home').hover(function (event) {
         event.preventDefault();
-        $('.home-text').toggleClass("animate__lightSpeedInLeft");
+        $('.home-text').addClass("animate__lightSpeedInLeft");
       });
       $('.home').hover(function (event) {
         event.preventDefault();
-        $('.home-text2').toggleClass("animate__lightSpeedInRight");
+        $('.home-text2').addClass("animate__lightSpeedInRight");
       });
     $('.about').hover(function (event) {
         event.preventDefault();
-        $('.about h1').toggleClass("animate__bounceIn");
+        $('.about h1').addClass("animate__bounceIn");
       });
       $('.skills').hover(function (event) {
         event.preventDefault();
-        $('.skills h1').toggleClass("animate__bounceIn");
+        $('.skills h1').addClass("animate__bounceIn");
       });
       $('.project').hover(function (event) {
         event.preventDefault();
-        $('.project>h1').toggleClass("animate__bounceIn");
+        $('.project>h1').addClass("animate__bounceIn");
         $('.content').addClass("animate__fadeIn");
       });
 
