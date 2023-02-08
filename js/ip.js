42aaa91f5e387c940346c58eb42e0e40ae5aefee
SC1314520 <blc0000421@gmail.com>
2023-02-08 21:45:13 +0800

    紀錄IP功能

--- js/ip.js
+++ js/ip.js
@@ -1,15 +1,15 @@
 
 
-function getIP(){
-    fetch('https://api.ipify.org?format=json')
-    .then(resp=>{
-        return resp.json();
-    })
-    .then(data=>
-        document.getElementById("ip-text").innerText=data.ip
-    )
-}
-getIP();
+// function getIP(){
+//     fetch('https://api.ipify.org?format=json')
+//     .then(resp=>{
+//         return resp.json();
+//     })
+//     .then(data=>
+//         document.getElementById("ip-text").innerText=data.ip
+//     )
+// }
+// getIP();
 
 
 function getIP2(){
@@ -21,7 +21,23 @@ function getIP2(){
     .then(function(data){
             document.getElementById("ip-text").innerText=data.ip;
             ip=data.ip;
+            recordIP(ip);
             console.log(ip)
         }
     )
 }
+
+function recordIP(ip){
+    console.log("record")
+    var formData=new FormData();
+    formData.append('ip',ip)
+    var config={
+        method:'POST',
+        body:formData
+    }
+    fetch('https://script.google.com/macros/s/AKfycbyxfvlmgNQhpOmindNbF7XqEuR5DB0qZCDJEyWm1LvLU-GK5oMeQ41u3reAeKu-2b0kYA/exec',config)
+    .then(resp=>console.log(resp))
+    .catch(error=>console.log(error));
+}
+
+getIP2();
